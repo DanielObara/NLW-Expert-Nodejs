204f16f55624d9747c588e8407830f28b8c0842d
Daniel Obara <daniel.tsutomu@hotmail.com>
2024-03-15 17:04:48 -0300

    feat: add redis and get all polls endpoint

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,8 +1,21 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
+	app.get("/polls", async (request, reply) => {
+		const poll = await prisma.poll.findMany();
+
+		if (!poll) {
+			return reply.status(404).send({ error: "Poll not found" });
+		}
+
+		return reply.send({
+			poll,
+		});
+	});
+
 	app.get("/polls/:pollId", async (request, reply) => {
 		const getPollParams = z.object({
 			pollId: z.string().uuid(),
@@ -24,6 +37,29 @@ export async function getPoll(app: FastifyInstance) {
 			},
 		});
 
-		return reply.send({ poll });
+		if (!poll) {
+			return reply.status(404).send({ error: "Poll not found" });
+		}
+
+		const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+		const votes = result.reduce((obj, line, index) => {
+			if (index % 2 === 0) {
+				obj[line] = Number(result[index + 1]);
+			}
+			return obj;
+		}, {} as Record<string, number>);
+
+		return reply.send({
+			poll: {
+				id: poll.id,
+				title: poll.title,
+				options: poll.options.map((option) => ({
+					id: option.id,
+					title: option.title,
+					score: option.id in votes ? votes[option.id] : 0,
+				})),
+			},
+		});
 	});
 }
